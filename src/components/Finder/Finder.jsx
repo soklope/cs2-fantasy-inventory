9bc8770beb35f9caa805ece8750b44427e672f67
soklope <skp2104@hotmail.com>
2025-04-22 13:45:40 +0200

    Add filter improvements

--- src/components/Finder/Finder.jsx
+++ src/components/Finder/Finder.jsx
@@ -2,29 +2,12 @@ import './finder.scss'
 import React, { useState, useEffect } from 'react';
 import SkinCard from '../SkinCard/SkinCard';
 import useInventoryStore from '../../store/inventoryStore';
+import sortByRarity from '../../helpers/sortFinder';
 
 export default function Finder() {
     const { itemInFocus, finderIsOpen, setFinderStatus, currentFaction } = useInventoryStore();
     const [skins, setSkins] = useState([]);
-
-    const rarityOrder = [
-      "#eb4b4b",
-      "#d32ce6",
-      "#8847ff",
-      "#4b69ff", 
-      "#5e98d9"  
-    ];
-
-    // const sortByRarity = () => {
-    //   const sorted = [...skins].sort((a, b) => {
-    //     const indexA = rarityOrder.indexOf(a.rarity.color || a.rarity);
-    //     const indexB = rarityOrder.indexOf(b.rarity.color || b.rarity);
-    
-    //     return (indexA === -1 ? Infinity : indexA) - (indexB === -1 ? Infinity : indexB);
-    //   });
-    
-    //   setSkins(sorted);
-    // };
+    const [order, setOrder] = useState("asc")
 
     useEffect(() => {
       let storedSkins = null;
@@ -58,15 +41,31 @@ export default function Finder() {
         matchedSkins = nameMatches;
       }
 
-      const sorted = [...matchedSkins].sort((a, b) => {
-        const indexA = rarityOrder.indexOf(a.rarity.color || a.rarity);
-        const indexB = rarityOrder.indexOf(b.rarity.color || b.rarity);
-    
-        return (indexA === -1 ? Infinity : indexA) - (indexB === -1 ? Infinity : indexB);
-      });
-      
-      setSkins(sorted);
+      setSkins(sortByRarity(matchedSkins, "asc"));
+
     }, [itemInFocus]);
+
+    useEffect(() => {
+      if (finderIsOpen) {
+        document.body.style.overflow = "hidden";
+      } else {
+        document.body.style.overflow = "";
+      }
+    
+      return () => {
+        document.body.style.overflow = "";
+      };
+    }, [finderIsOpen]);
+
+    const changeOrder = () => {
+      if (order === "asc") {
+        setSkins(sortByRarity(skins, "desc")),
+        setOrder("desc")
+      } else (
+        setSkins(sortByRarity(skins, "asc")),
+        setOrder("asc")
+      )
+    }
     
     return (
         <>
@@ -75,7 +74,7 @@ export default function Finder() {
                     <div className='finder page-container'>
 
                       <div className='finder__header'>
-                        <button className='finder__sort-button' onClick={() => sortByRarity()}>sort by: rarity</button>
+                        <button className='finder__sort-button' onClick={() => changeOrder()}>sort by rarity: <span>{order}</span></button>
                         <button className='button-cancel' onClick={() => setFinderStatus('', '', null)}></button>
                       </div>
 
---
+++ src/helpers/sortFinder.js
@@ -0,0 +1,25 @@
+const rarityOrder = [
+    "#eb4b4b", // red
+    "#d32ce6", // pink/purple
+    "#8847ff", // purple
+    "#4b69ff", // blue
+    "#5e98d9"  // light blue
+];
+
+/**
+ * Sorts an array of skins by rarity color, in ascending or descending order.
+ * @param {Array} skinArray - The array of skins to sort.
+ * @param {'asc' | 'desc'} order - The sort order. Default is 'asc'.
+ */
+export default function sortByRarity(skinArray = [], order = 'asc') {
+    const sorted = [...skinArray].sort((a, b) => {
+        const indexA = rarityOrder.indexOf(a.rarity.color || a.rarity);
+        const indexB = rarityOrder.indexOf(b.rarity.color || b.rarity);
+        const safeIndexA = indexA === -1 ? Infinity : indexA;
+        const safeIndexB = indexB === -1 ? Infinity : indexB;
+
+        return safeIndexA - safeIndexB;
+    });
+
+    return order === 'desc' ? sorted.reverse() : sorted;
+}
